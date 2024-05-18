55d7c0adbc09dd0fe1d004c77ed6ca90a3259293
Dat Nguyen <datn1101@gmail.com>
2024-05-18 16:20:27 -0400

    Added Feed component

---
+++ src/components/Feed.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Post from './Post';
+
+function Feed({ posts }) {
+  return (
+    <div>
+      {posts.map((post, index) => (
+        <Post
+          key={index}
+          title={post.title}
+          content={post.content}
+          likes={post.likes}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Feed;
