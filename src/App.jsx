f0b21519da27c1a6fd338237699475e03e1a9fc2
Dat Nguyen <datn1101@gmail.com>
2024-05-18 16:21:23 -0400

    Updated App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Post from './components/Post';
-function App(){ 
-  return(
+import CreatePostForm from './components/CreatePostForm';
+import Feed from './components/Feed';
+
+function App() {
+  const posts = [
+    { title: 'Post 1', content: 'Content for post 1', likes: 10 },
+    { title: 'Post 2', content: 'Content for post 2', likes: 20 },
+  ];
+
+  return (
     <div>
       <h1>Fakebook!</h1>
-      
-        {/* CreatePostForm */}
-        
-        {/* Feed */}
-        
-        <Post content="This is a test post!" />
-        <Post content="This is another test post!" />
-    
+      <CreatePostForm />
+      <Feed posts={posts} />
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
